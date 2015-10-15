f6ca4fa7c4391948531fc4db5d89c823cf9c9acc
zhangke <zhangke@zhangkedeimac.local>
2015-10-15 11:18:54 +0800

    Initial Commit

---
+++ SortForOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SortForOC
+//
+//  Created by zhangke on 15/10/15.
+//  Copyright © 2015年 zhangke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SortForOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SortForOC
+//
+//  Created by zhangke on 15/10/15.
+//  Copyright © 2015年 zhangke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
